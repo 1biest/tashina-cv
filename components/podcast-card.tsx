32790f43152aae636df64703b98e19cfb1af2343
biest <93846276+1biest@users.noreply.github.com>
2022-11-20 20:55:04 -0700

    alignment, light theme

--- components/podcast-card.tsx
+++ components/podcast-card.tsx
@@ -10,7 +10,7 @@ const PodcastCard = (props) => {
                     <img src={content.thumb} className="bg-cover h-40 m-auto bg-center bg-100"/>
                 </div>
                 <h3 className='text-xl font-semibold text-center'>{content.title}</h3>
-                <p className='text-sm font-extralight'>{content.desc}</p>
+                <p className='text-sm'>{content.desc}</p>
                 <div className='flex justify-end'>
                   <button className='justify-self-end bg-gradient-to-tl from-[rgba(30,40,60,0.5)] to-transparent backdrop-blur-lg px-5 py-2 rounded-lg mt-5 uppercase font-bold text-xs tracking-widest cursor-pointer'>{content.link}</button>
                 </div>
--- components/podcast.tsx
+++ components/podcast.tsx
@@ -7,7 +7,7 @@ const podcast = () => {
     <>
         <div className='z-20 relative'>
             <h3 className='text-2xl font-light  text-center'>Podcast</h3>
-            <p className='text-sm font-extralight pt-4 max-w-[100%] text-center'>Go in depth with us as we discuss what crypto and the cosmos is all about.<br/>The unique features and abilities of this new technology.</p>
+            <p className='text-sm pt-4 max-w-[100%] text-center'>Go in depth with us as we discuss what crypto and the cosmos is all about.<br/>The unique features and abilities of this new technology.</p>
             <div className='grid grid-cols-1 md:grid-cols-3 mt-10 gap-4'> 
                 <PodcastCard data={data} />
             </div>
